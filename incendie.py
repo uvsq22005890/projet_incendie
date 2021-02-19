36f77900c09add13a751dbc53edf46b4a25a2e45
Prénom NOM <prenom.nom@ens.uvsq.fr>
2021-02-19 15:35:03 +0100

    CONTRAINTES PROGRAMMATION

--- incendie.py
+++ incendie.py
@@ -8,3 +8,17 @@
 #PERICAT CLEMENT
 #https://github.com/uvsq22005890/projet_incendie
 #########################################
+
+# CONTRAINTES PROGRAMMATION : 
+# Programme écrit dans fichier unique : incendie.py
+# Programme doit pas définir classes objets
+# Utiliser librairie : tkinter
+# Utiliser des variables globales
+
+# Programme découpé de la manière suivante (chaque partie doit apparaître bien distinctement en utilisant des commentaires) :#
+# Informations liées au groupe
+# import des librairies
+# Constantes écrties en MAJUSCULE
+# Définir variables globales
+# Définir fonctions : Fonction doit contenir docstring
+# Programme principal doit contenir définition widgets et événements qui leur sont liés et l’appel à la boule de gestion des événements
